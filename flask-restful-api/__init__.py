464a39f8816ca79232cd379e6edeab3c82132332
RoseBud13 <xiongkaijie13@gmail.com>
2021-05-18 13:14:07 +0800

    update

--- flask-restful-api/__init__.py
+++ flask-restful-api/__init__.py
@@ -3,12 +3,27 @@ import os
 import sys
 
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from flask.views import MethodView
 from flask_login import LoginManager
 
 import commands
 
+
+# configuration
+DEBUG = True
+
+
+# instantiate the app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+
+# init database
 # SQLite URI compatible
 WIN = sys.platform.startswith('win')
 if WIN:
@@ -16,14 +31,20 @@ if WIN:
 else:
     prefix = 'sqlite:////'
 
-app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
+# instantiate the database
 db = SQLAlchemy(app)
+
+
+# instantiate the login extension
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -31,4 +52,3 @@ def load_user(user_id):
     user = User.query.get(int(user_id))
     return user
 
-login_manager.login_view = 'login'
--- flask-restful-api/app.py
+++ flask-restful-api/app.py
@@ -1,23 +1,31 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+# -*- coding: utf-8 -*-
+import os
 
+from flask import Flask, jsonify, request
 
-# configuration
-DEBUG = True
-
-# instantiate the app
-app = Flask(__name__)
-app.config.from_object(__name__)
-
-# enable CORS
-CORS(app, resources={r'/*': {'origins': '*'}})
-
+from __init__ import app, db
+from models import Resource
 
 # sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
 
+@app.route('/resource', methods=['GET', 'POST'])
+def all_resources():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        Resource.append({
+            'resource_name': post_data.get('resource_name'),
+            'status': post_data.get('status'),
+            'description': post_data.get('description'),
+            'resource_type': post_data.get('resource_type')
+        })
+        response_object['message'] = 'Resource added!'
+    else:
+        response_object['resources'] = Resource
+    return jsonify(response_object)
 
 if __name__ == '__main__':
     app.run()
